f8101508d88495782a6d837b922a7dc63421a2bf
Osmar Junior <oj13black@hotmail.com>
2021-05-05 22:47:47 -0300

    add exercicios

---
+++ Udemy/002/exercicios/src/App.js
@@ -0,0 +1,9 @@
+/* eslint-disable prettier/prettier */
+import React from 'react'
+import { Text } from 'react-native'
+
+function App(){
+    return <Text>Meu Primeiro Componente</Text>
+}
+
+export default App
