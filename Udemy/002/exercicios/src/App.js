9563f84635e237c9c84e1677eeeb11de948f81d8
Osmar Junior <oj13black@hotmail.com>
2021-05-27 23:13:46 -0300

    manipulação de componentes

--- Udemy/002/exercicios/src/App.js
+++ Udemy/002/exercicios/src/App.js
@@ -1,7 +1,8 @@
 /* eslint-disable prettier/prettier */
 import React from 'react';
-import { SafeAreaView } from 'react-native';
+import { SafeAreaView, Text } from 'react-native';
 // import { Text } from 'react-native';
+import CompPadrao, { Comp1, Comp2 } from './components/Multi'
 import  Primeiro from './components/Primeiro';
 
 // function App() {
@@ -12,6 +13,12 @@ import  Primeiro from './components/Primeiro';
 
 export default () => (
     <SafeAreaView>
+        <CompPadrao/>
+        <Comp1 />
+        <Comp2 />
+        <Text>
+            { 1 + 1}
+        </Text>
         <Primeiro />
     </SafeAreaView>
 );
